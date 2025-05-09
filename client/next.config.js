fd953eb87036eede239d6d7f93f8fc4179813af5
hecky559 <33211197+hecky559@users.noreply.github.com>
2025-05-09 07:06:48 -0700

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  },
-};
-
-module.exports = nextConfig;
+const 
