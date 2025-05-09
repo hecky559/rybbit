20d62cf08360425d555582003614f7841c46d9ee
hecky559 <33211197+hecky559@users.noreply.github.com>
2025-05-09 06:29:25 -0700

    Update createAccount.ts

--- server/src/api/user/createAccount.ts
+++ server/src/api/user/createAccount.ts
@@ -1,10 +1,1 @@
-i/ this can also be an array for multiple roles (e.g. ["admin", "sale"])
-      },
-    });
-
-    return reply.status(201).send({
-      message: "Account created successfully",
-      user: {
-        id: result.user.id,
-        email: result.user.email,
-      
+i/ t
