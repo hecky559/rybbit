379ee18587ff88885a5572d3096fe128305ff092
hecky559 <33211197+hecky559@users.noreply.github.com>
2025-05-09 06:31:12 -0700

    Update addUserToOrganization.ts

--- server/src/api/user/addUserToOrganization.ts
+++ server/src/api/user/addUserToOrganization.ts
@@ -1,22 +1,2 @@
 
-    role: string;
-    organizationId: string;
-  };
-}
-
-export async function addUserToOrganization(
-  request: FastifyRequest<AddUserToOrganization>,
-  reply: FastifyReply
-) {
-  try {
-    const { email, role, 
-    ]);
-
-    return reply.status(201).send({
-      message: "User added to organization successfully",
-    });
-  } catch (error: any) {
-    console.error(String(error));
-    return reply.status(500).send({ error: String(error) });
-  }
 }
